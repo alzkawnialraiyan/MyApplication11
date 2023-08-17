628a7f7dfb537f1e0540969ced168253398e0170
al raiyan al zakwani <alzakwanialraiyan0@gmail.com>
2023-08-17 12:38:03 +0400

    change

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +19,11 @@ public class MainActivity extends AppCompatActivity {
         Spinner age = findViewById(R.id.age);
         Spinner phone = findViewById(R.id.phone);
 
-        ArrayAdapter<CharSequence>adapter1=ArrayAdapter.createFromResource(this, R.array.gender, android.R.layout.simple_spinner_item);
-        age.setAdapter(adapter1);
+        ArrayAdapter<CharSequence> arrayadapter = ArrayAdapter.createFromResource(this, R.array.gender, android.R.layout.simple_spinner_item);
+        age.setAdapter(arrayadapter);
 
         ArrayAdapter<CharSequence>adapter=ArrayAdapter.createFromResource(this, R.array.phonenum, android.R.layout.simple_spinner_item);
         phone.setAdapter(adapter);
-
 
     }
 }
